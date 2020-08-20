926d442c688970bbe61cdaf7a3ef9cb15055c14b
GauravAmarnaniTest <gauravamarnani12@gmail.com>
2020-08-20 20:37:33 +0530

    Added AppletDemo.java.

---
+++ Java/AWT/AppletDemo.java
@@ -0,0 +1,34 @@
+//Author : Gaurav Amarnani.
+
+import java.awt.*;
+import java.applet.*;
+import static java.lang.System.out;
+
+/*
+<applet code="AppletDemo.class" width="100" height="100">
+</applet>
+*/
+
+public class AppletDemo extends Applet 
+{
+	public void init()
+	{
+		out.println("----->init().");
+	}
+	public void start()
+	{
+		out.println("----->start().");
+	}
+	public void paint(Graphics g)
+	{
+		out.println("----->paint().");
+	}
+	public void stop()
+	{
+		out.println("----->stop().");
+	}
+	public void destroy()
+	{
+		out.println("----->destroy().");
+	}
+}
